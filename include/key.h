9443c4100e19ac27f36a088e723fe880302f06be
blmayer <bleemayer@gmail.com>
2018-03-30 03:04:33 +0000

    Unified key includes into one file.

---
+++ include/key.h
@@ -0,0 +1,10 @@
+/*
+ *  In this file we place the most important part of the project: the key.
+ */
+
+#ifndef KEY_H
+#define KEY_H
+#define KEY "blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#blmayer#"
+
+#endif
+
